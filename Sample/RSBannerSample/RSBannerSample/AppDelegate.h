1cc441545b3177f07eecaf58f8e8fc989d0f1b9c
R0CKSTAR <yeahdongcn@gmail.com>
2017-04-09 20:53:49 +0800

    Add a sample project

---
+++ Sample/RSBannerSample/RSBannerSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RSBannerSample
+//
+//  Created by 叶晓冬 on 2017/4/9.
+//  Copyright © 2017年 R0CKSTAR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
